141593bd8419de716cfc2298c8d3d1153b2fbff0
student <student@vlab.local>
2022-12-05 16:13:03 +0100

    Eliptical Circle but it still has some bugs

---
+++ Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics_animation
+{
+    public partial class Form2 : Form
+    {
+        Bitmap bitmap;
+        public Form2()
+        {
+            InitializeComponent();
+            bitmap  = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+
+            Graphics.drawCircle(e.X, e.Y, 50, 100, bitmap, pictureBox1, Color.Red);
+        }
+    }
+}
--- Graphics.cs
+++ Graphics.cs
@@ -2,6 +2,46 @@ namespace graphics_animation
 {
     internal static class Graphics
     {
+        public static void drawCircle(int x,int y, int minRadius,int maxRadius,Bitmap bitmap, PictureBox pictureBox, Color color)
+        {
+            int with = bitmap.Width;
+            int height = bitmap.Height;
+            int x1, y1;
+            int intersection;
+            float coeficient;
+            int radius = minRadius;
+            bool radiusIncreasingMode = true;
 
+            if (minRadius <= 50)
+            {
+                intersection = 360;
+                coeficient = 1;
+            }
+            else if(minRadius <= 100)
+            {
+                intersection = 720;
+                coeficient = 0.5f;
+            }
+            else
+            {
+                intersection = 1440;
+                coeficient = 0.25f;
+            }
+
+            for(int i = 0; i < intersection; i++)
+            {
+                x1 = (int)(x + radius * Math.Cos(i * coeficient * Math.PI / 180));
+                y1 = (int)(y + radius * Math.Sin(i * coeficient * Math.PI / 180));
+                bitmap.SetPixel(x1, y1, color);
+
+               if (radius == maxRadius || radius == minRadius)
+                {
+                    radiusIncreasingMode = !radiusIncreasingMode;
+                }
+                radius += radiusIncreasingMode ? 1 : -1;
+
+            }
+            pictureBox.Image = bitmap;
+        }
     }
 }
